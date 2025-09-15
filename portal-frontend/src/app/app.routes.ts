ff6bbea890a9574d7b34c45e5ae78cbb2704e472
olucasmatheus <lucasmapb@gmail.com>
2025-09-15 00:31:58 -0300

    Landing page primeira fase

--- portal-frontend/src/app/app.routes.ts
+++ portal-frontend/src/app/app.routes.ts
@@ -5,18 +5,19 @@ import { InternsComponent } from './pages/interns/interns.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { RegisterInternComponent } from './pages/register-intern/register-intern.component';
 import { adminGuard } from './guards/admin.guard';
-
+import { ApresentacaoComponent } from './pages/apresentacao/apresentacao';
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
   { path: 'interns', component: InternsComponent },
   { path: 'register', component: RegisterComponent },
+  {path: 'apresentacao', component: ApresentacaoComponent},
   {
     path: 'register-intern',
     component: RegisterInternComponent,
     canActivate: [adminGuard],
   }, // guardian para proteger a rota
   //rotas de redirecionamento
-  { path: '', redirectTo: 'login', pathMatch: 'full' }, //rota padrão
-  { path: '**', redirectTo: 'login' }, //rota coringa
+  { path: '', redirectTo: 'apresentacao', pathMatch: 'full' }, //rota padrão
+  { path: '**', redirectTo: 'apresentacao' }, //rota coringa
 ];
---
+++ portal-frontend/src/app/pages/apresentacao/apresentacao.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-apresentacao',
+  imports: [],
+  templateUrl: './apresentacao.html',
+  styleUrl: './apresentacao.scss'
+})
+export class ApresentacaoComponent {
+
+}
