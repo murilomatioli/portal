6a9eb693708e29755e56d9fe94cdd96e0c89326c
olucasmatheus <lucasmapb@gmail.com>
2025-09-15 07:41:23 -0300

    Alteração nas rotas e na estilização

--- portal-frontend/src/app/pages/apresentacao/apresentacao.ts
+++ portal-frontend/src/app/pages/apresentacao/apresentacao.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-apresentacao',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
+  standalone: true,
   templateUrl: './apresentacao.html',
   styleUrl: './apresentacao.scss'
 })
 export class ApresentacaoComponent {
+  isMenuOpen = false;
 
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
