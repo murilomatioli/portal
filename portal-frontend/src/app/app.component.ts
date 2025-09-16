2ceec8236781989b51d52860a4d66120c7fb9111
olucasmatheus <lucasmapb@gmail.com>
2025-09-16 07:09:40 -0300

    Implementação de botao para tela inicial que aparece nas outras telas

--- portal-frontend/src/app/app.component.ts
+++ portal-frontend/src/app/app.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, RouterOutlet, Router, RouterLink } from '@angular/router';
 import { ApiService, Estagiario } from './services/api.service';
-import { NgIf, NgFor } from '@angular/common'; //mostrar os dados no HTML
+import { NgIf, NgFor, CommonModule } from '@angular/common'; //mostrar os dados no HTML
+import { filter } from 'rxjs';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, RouterLink, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  constructor() {
-    console.log('teste de componente - inicialização');
+  showHeader = false;
+  
+  constructor(private router: Router) {
+    this.router.events.pipe(
+      filter((event):event is NavigationEnd => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => { //filtra os eventos de navegacao concluida
+      if (event.urlAfterRedirects === '/'){
+        this.showHeader = false;
+      } else {
+        this.showHeader = true;
+      }
+    });
   }
+
 }
